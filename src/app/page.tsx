6fee13fa3dcfb627e5908d6ef4d2aad8b5afe230
Nguyen Xuan Trong <trongnx@gofiber.vn>
2025-12-15 08:29:34 +0700

    chore: global css config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import CounterTest from '@/components/CounterTest';
 import UserList from '@/components/UserList';
 import { getPosts } from '@/shared/endpoints/post.endpoint';
 import { getPartnerConfig } from '@/shared/endpoints/partner-domain.endpoint';
+import Image from 'next/image';
 
 export default async function Home() {
   const posts = await getPosts();
   const partnerConfig = await getPartnerConfig();
   return (
-    <div className='bg-gray pt-10 pb-10 md:pt-14 md:pb-14 lg:pt-20 lg:pb-18'>
-      <div className='mb-8 flex flex-col items-center justify-center gap-8'>
-        <CounterTest />
-        <UserList />
+    <div className='pt-10 pb-10 md:pt-14 md:pb-14 lg:pt-20 lg:pb-18'>
+      <div className='bg-primary container mb-8 flex h-10 flex-col items-center justify-center gap-8'>
+        <Image
+          width={1200}
+          height={1200}
+          src='https://resource.medpro.com.vn/static/images/bvmathcm/web/slide.png?t=21084.808919858144'
+          alt='slide'
+        />
       </div>
     </div>
   );
